a5801310a88e0f008b22c2b4a52c7be79ef68b62
andres-odoo <andres.odoo.dev@gmail.com>
2023-06-12 22:12:13 -0500

    Comment out require dotenv in dynamo.js and added it to server.js to cover both dynamo and mongo

--- dynamo.js
+++ dynamo.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 const {v4:uuidv4} = require("uuid");
-require("dotenv").config();
+// require("dotenv").config();
 
 
 AWS.config.update({
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 const { readNotesMongo, createNoteMongo, deleteNoteMongo } = require("./mongo");
 const { readNotesDynamo, createNoteDynamo, deleteNoteDynamo } = require("./dynamo");
 
